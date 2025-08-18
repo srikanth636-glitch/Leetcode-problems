0580af2013a4001244fb58bb48b844bdb1a3c874
srikanth636-glitch <mudavaths636@gmail.com>
2025-08-18 11:01:30 +0530

    Time: 1 ms (99.70%) | Memory: 46.3 MB (53.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -4,14 +4,11 @@ class Solution {
         int p1 = 0;
         int p2 = 0;
         while( p2 < n) {
-            if (nums[p2] == 0){
-                p2++;
-            }
-            else{
+            if (nums[p2] != 0) {
                 nums[p1] = nums[p2];
                 p1++;
-                p2++;
             }
+            p2++;
         }
         while(p1 < n) {
             nums[p1] = 0;
